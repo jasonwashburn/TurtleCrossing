e4d294bd3621e249fe5830b511e6ab806ab1cabd
Jason Washburn <wburn@wburn.net>
2021-02-27 08:00:33 -0600

    adds car generation and removal

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,37 @@
+from turtle import Turtle
+from random import choice, randint
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+
+    def __init__(self):
+        self.car_list = []
+        self.new_car()
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def new_car(self):
+        new_car = Turtle(shape='square')
+        new_car.color(choice(COLORS))
+        new_car.penup()
+        new_car.shapesize(stretch_len=2)
+        new_car.setx(280)
+        new_car.sety(randint(-250, 250))
+        self.car_list.append(new_car)
+
+    def move_cars(self):
+        for car in self.car_list:
+            new_x = car.xcor() - self.car_speed
+            car.setx(new_x)
+
+    def flush_cars(self):
+        for car in self.car_list:
+            if car.xcor() < -320:
+                self.car_list.remove(car)
+                print('removed car')
+
+    def speed_up(self):
+        self.car_speed += MOVE_INCREMENT
--- main.py
+++ main.py
@@ -10,10 +10,12 @@ screen.tracer(0)
 
 player = Player()
 
+car_manager = CarManager()
+
 screen.listen()
 screen.onkey(player.move_up, 'Up')
 
-
+new_car_count = 0
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
@@ -22,3 +24,16 @@ while game_is_on:
     # Detect when player reaches the top of the screen
     if player.ycor() > 300:
         player.refresh()
+
+    # Create new car every 6 times the screen is refreshed
+    if new_car_count == 6:
+        car_manager.new_car()
+        new_car_count = 0
+
+    # Move the cars
+    car_manager.move_cars()
+
+    # Remove cars when they pass the edge of the screen
+    car_manager.flush_cars()
+
+    new_car_count += 1
